8010d2eedfb38f8081ee9243befccac9234e61c7
William <w-wang14@mails.tsinghua.edu.cn>
2017-08-16 11:12:33 -0400

    Generate word embeddings and visualize them.

--- src/Main.py
+++ src/Main.py
@@ -3,16 +3,62 @@
 from Initializer import getText
 from EMStep import EMIter
 from Score import calcScore, calcFreq
+import math
+
+tauL = 5
+tauF = 5
 
 text = getText('chapter1.txt')
-dictionary = EMIter(5, 5, text)
+dictionary = EMIter(tauL, tauF, text)
 # final_dictionary = calcScore(text, dictionary)
 temp = []
 temp.append(dictionary)
 prep_all_dict = calcFreq(temp)
 final_dictionary = prep_all_dict[0]
 
-final_dictionary = sorted(final_dictionary.items(), key=lambda d: d[1], reverse=True)
-for item in final_dictionary:
-    if len(item[0]) > 1:
-        print item[0], ':', item[1]
+# final_dictionary = sorted(final_dictionary.items(), key=lambda d: d[1], reverse=True)
+# for item in final_dictionary:
+#     if len(item[0]) > 1:
+#         print item[0], ':', item[1]
+
+
+def generator(sentence, dictionary, sent_dict):
+	length = len(sentence)
+	if length == 1:
+		return math.log(dictionary[sentence]), [sentence]
+	if sentence in sent_dict:
+		return sent_dict[sentence]
+	tempMax = -500
+	words = []
+	for i in range(0, tauL):
+		if length > i:
+			tempWord = sentence[0:i]
+			if tempWord in dictionary.keys():
+				tempTheta, _words = generator(sentence[i:length], dictionary, sent_dict)
+				tempTheta += math.log(dictionary[tempWord])
+				if tempTheta > tempMax:
+					tempMax = tempTheta
+					words = [tempWord]
+					words = words + _words
+		else:
+			break
+	theta = tempMax
+	sent_dict[sentence] = [theta, words]
+
+	return theta, words
+
+def splitor(text, dictionary):
+	ans = []
+	for sentence in text:
+		sent_dict = {}
+		_, words = generator(sentence, dictionary, sent_dict)
+		ans = ans + words
+
+	return ans
+
+# theta, words = generator(text[48], final_dictionary)
+# for item in words:
+# 	print item
+dddd = splitor(text, final_dictionary)
+# for item in dddd:
+# 	print item
--- src/test.py
+++ src/test.py
@@ -1,12 +1,12 @@
-# coding=utf-8
-
-import copy
-
-def ttttt(dict):
-    local_dict = copy.copy(dict)
-    if '1' in local_dict:
-        local_dict['1'] = 0
-
-a = {'1':23, '2':34}
-ttttt(a)
-print a
+#coding:utf-8  
+import matplotlib  
+# matplotlib.use('qt4agg')  
+from matplotlib.font_manager import *  
+import matplotlib.pyplot as plt  
+#定义自定义字体，文件名从1.b查看系统中文字体中来  
+myfont = FontProperties(fname='/usr/share/fonts/msyh.ttf/msyh.ttf')  
+#解决负号'-'显示为方块的问题  
+matplotlib.rcParams['axes.unicode_minus']=False  
+plt.plot([-1,2,-5,3])  
+plt.title(u'中文',fontproperties=myfont)  
+plt.show()
